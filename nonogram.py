5409680620209905878e31945278162d2d4093df
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2020-11-29 11:22:02 -0500

    Partial validation done

--- nonogram.py
+++ nonogram.py
@@ -186,4 +186,4 @@ if __name__ == "__main__":
     cols, rows = read_file(sys.argv[1])
     #solveNonogram(cols,rows)
     print(isValidNonogram(M, cols, rows))
-    #write_file(M, sys.argv[2])
+    write_file(M, sys.argv[2])
